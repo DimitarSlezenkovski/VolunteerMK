66170b6412669e3ef4c15d7f7b8e2f258ccfcbcb
DimitarSlezenkovski <dslezenkovski@gmail.com>
2021-07-20 14:58:57 -0700

    initial commit

---
+++ Data/CommonData/VolunteerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using VolunteerMK.Models;
+
+namespace VolunteerMK.Data
+{
+    public class VolunteerDbContext : ApiAuthorizationDbContext<User>
+    {
+        public VolunteerDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolunteerMK.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public float Rating { get; set; }
+        public string Role { get; set; }
+    }
+}
